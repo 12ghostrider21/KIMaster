9c253cea41ce43df49c89e69e009c93a05963894
Flying-Suricate <suricate2505@gmail.com>
2024-05-17 16:02:04 +0200

    import and folder changes for docker

--- GameClient/StartClient.py
+++ GameClient/StartClient.py
@@ -5,9 +5,14 @@ from game_client import GameClient
 
 def main():
     # Extract command-line arguments or use default values
-    port = int(os.environ["PORT_TO_GAME_CLIENT"])
-    host = os.environ["HOST_SOCKET_SERVER"]
-    key = os.environ["LOBBY_KEY"]
+    try:
+        port = int(os.environ["PORT_TO_GAME_CLIENT"])
+        host = os.environ["HOST_SOCKET_SERVER"]
+        key = os.environ["LOBBY_KEY"]
+    except Exception:
+        port = 12345
+        host = "0.0.0.0"
+        key = ""
 
     game_client = GameClient(port=port, host=host, key=key).run()
     asyncio.get_event_loop().run_until_complete(game_client)
