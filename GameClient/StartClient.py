212e6a1dc537613de56c8a6366e4c27eae4ce7fd
Flying-Suricate <suricate2505@gmail.com>
2024-05-17 15:42:09 +0200

    StartClient.py changed for docker

--- GameClient/StartClient.py
+++ GameClient/StartClient.py
@@ -1,23 +1,18 @@
 import asyncio
-import sys
+import os
 from game_client import GameClient
 
 
 def main():
     # Extract command-line arguments or use default values
-    port = sys.argv[1] if len(sys.argv) > 1 else "12345"
-    port = int(port)
-    host = sys.argv[2] if len(sys.argv) > 2 else "localhost"
-    key = sys.argv[3] if len(sys.argv) > 3 else "e318ad20ae5b57426f8970a2fe644942740d2c1e2554c327ff4449029b0109c1"
+    port = int(os.environ["PORT_TO_GAME_CLIENT"])
+    host = os.environ["HOST_SOCKET_SERVER"]
+    key = os.environ["LOBBY_KEY"]
 
     game_client = GameClient(port=port, host=host, key=key).run()
     asyncio.get_event_loop().run_until_complete(game_client)
 
 
 if __name__ == "__main__":
-    if len(sys.argv) == 1 or len(sys.argv) == 4:
-        main()
-    else:
-        # Print usage and exit if arguments are incorrect
-        print("Usage: python StartClient.py port host key")
-        sys.exit(1)
+    main()
+
