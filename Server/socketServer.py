9e6d9e87156eb3bf15b74ae0397e46b52f91280f
Flying-Suricate <suricate2505@gmail.com>
2024-06-13 14:46:48 +0200

    Starting with dynamic import module

--- Server/socketServer.py
+++ Server/socketServer.py
@@ -1,32 +1,15 @@
-import base64
-import importlib.util
 import io
-import json
 import os
-import struct
-import sys
 
 import numpy as np
 import pygame
 from fastapi import WebSocket, WebSocketDisconnect
 
-from Games.connect4.Connect4Game import Connect4Game
-from Games.connect4.keras.NNet import NNetWrapper
-from Tools.Game_Config.difficulty import EDifficulty
 from Tools.language_handler import LanguageHandler
-from Tools.mcts import MCTS
 from Tools.rcode import RCODE
-from Tools.utils import dotdict
 from connection_manager import AbstractConnectionManager
 from lobby import Lobby
 from lobby_manager import LobbyManager
-
-def init_nn(game, nnet, folder: str, file: str):
-    nn = nnet(game)
-    nn.load_checkpoint(folder, file)
-    args = dotdict({'numMCTSSims': 5, 'cpuct': 1.0})
-    mcts = MCTS(game, nn, args)
-    return mcts
 
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'    # disable some logging features
 
@@ -35,11 +18,8 @@ class SocketServer(AbstractConnectionManager):
     def __init__(self, msg_builder: LanguageHandler):
         super().__init__(msg_builder)
         self.manager: LobbyManager = LobbyManager()
-        self.imports: dict = self.find_and_import_classes("/app/Games")
-        self.lambdas: dict = self.create_lambda()
-        for k, v in self.lambdas.items():
-            for x, y in v.items():
-                print(k, x, y)
+        #self.imports: dict = self.find_and_import_classes("/app/Games")
+        #self.lambdas: dict = self.create_lambda()
 
     async def connect(self, websocket: WebSocket):
         query_params = websocket.query_params
@@ -62,118 +42,8 @@ class SocketServer(AbstractConnectionManager):
         byte_io.close()
         return png_bytes
 
-    @staticmethod
-    def import_class_from_file(filepath: str, class_name: str):
-        spec = importlib.util.spec_from_file_location(class_name, filepath)
-        module = importlib.util.module_from_spec(spec)
-        spec.loader.exec_module(module)
-
-        # Find the class with case-insensitive match
-        for attr in dir(module):
-            if attr.lower() == class_name.lower():
-                return getattr(module, attr)
-
-        raise AttributeError(f"Class {class_name} not found in {filepath}")
-
-    def find_and_import_classes(self, games_folder: str):
-        imports = {}
-
-        for game_name in os.listdir(games_folder):
-            game_path = os.path.join(games_folder, game_name)
-            if os.path.isdir(game_path):
-                imports[game_name] = {}
-
-                # Import Game classes (files ending with Game.py)
-                for file_name in os.listdir(game_path):
-                    if file_name.endswith('Game.py'):
-                        game_file = os.path.join(game_path, file_name)
-                        if os.path.isfile(game_file):
-                            try:
-                                game_class = self.import_class_from_file(game_file, file_name.replace(".py", ""))
-                                imports[game_name]['Game'] = game_class()
-                            except (FileNotFoundError, AttributeError) as e:
-                                print(f"Could not import Game class from {game_file}: {e}")
-
-                # Import NNetWrapper class from keras/NNet.py
-                keras_file = os.path.join(game_path, 'keras', 'NNet.py')
-                if os.path.isfile(keras_file):
-                    try:
-                        keras_class = self.import_class_from_file(keras_file, 'NNetWrapper')
-                        imports[game_name]['NNetWrapper'] = keras_class
-                    except (FileNotFoundError, AttributeError) as e:
-                        print(f"Could not import NNetWrapper class from {keras_file}: {e}")
-
-                # import Keras folder and model
-                keras_model = os.path.join(game_path, 'keras', 'best.h5')
-                if os.path.isfile(keras_model):
-                    file = os.path.basename(keras_model)
-                    folder = os.path.dirname(os.path.abspath(keras_model))
-                    imports[game_name]["folder"] = folder
-                    imports[game_name]["file"] = file
-
-                # Import NNetWrapper class from pytorch/NNet.py
-                pytorch_file = os.path.join(game_path, 'pytorch', 'NNet.py')
-                if os.path.isfile(pytorch_file):
-                    try:
-                        pytorch_class = self.import_class_from_file(pytorch_file, 'NNetWrapper')
-                        imports[game_name]['NNetWrapper'] = pytorch_class
-                    except (FileNotFoundError, AttributeError) as e:
-                        print(f"Could not import NNetWrapper class from {pytorch_file}: {e}")
-
-                # import pytorch folder and model
-                torch_model = os.path.join(game_path, 'pytorch', 'best.h5')
-                if os.path.isfile(torch_model):
-                    file = os.path.basename(torch_model)
-                    folder = os.path.dirname(os.path.abspath(torch_model))
-                    imports[game_name]["folder"] = folder
-                    imports[game_name]["file"] = file
-        return imports
-
-    def create_lambda(self):
-        print("Loading Ai models...")
-        results = {}
-        ignored_games = []
-        # save original stdout
-        original_stdout = sys.stdout
-        original_stderr = sys.stderr
-        try:
-            # redirect stdout to silent mode
-            sys.stdout = open(os.devnull, 'w')
-            sys.stderr = open(os.devnull, 'w')
-            for k, v in self.imports.items():
-                game = self.imports[k].get("Game")
-                network_class = self.imports[k].get("NNetWrapper")  # get the right NNet
-                h5_folder = self.imports[k].get("folder")  # get the .h5 trained model path
-                h5_file = self.imports[k].get("file")  # get the .h5 file
-                if h5_folder and h5_file:
-                    for difficulty in EDifficulty:
-                        mcts = self.init_nn(game, network_class, h5_folder, h5_file, difficulty)
-                        if k not in results:
-                            results[k] = {}
-                        results[k][difficulty] = lambda x: mcts.getActionProb(x, temp=0)
-                else:
-                    ignored_games.append(f"For '{game}' no best.h5 was found. Ignoring it.")
-        finally:
-            # set stdout back to original
-            sys.stdout = original_stdout
-            sys.stderr = original_stderr
-            print("Loading Ai models done.")
-        return results
-
-    def init_nn(self, game, nnet, folder: str, file: str, difficulty: EDifficulty) -> MCTS:
-        nn = nnet(game)
-        nn.load_checkpoint(folder, file)
-        args = dotdict({'numMCTSSims': difficulty.value, 'cpuct': 1.0})
-        mcts = MCTS(game, nn, args)
-        return mcts
-
     async def websocket_endpoint(self, websocket: WebSocket):
         await self.connect(websocket)
-        game = Connect4Game()
-        folder = r"/app/Games/connect4/keras"
-        file = "best.h5"
-        mcts = init_nn(game, NNetWrapper, folder, file)
-        func = lambda x: mcts.getActionProb(x, temp=0)
         try:
             while True:
                 read_object: dict = await websocket.receive_json()
@@ -203,37 +73,6 @@ class SocketServer(AbstractConnectionManager):
                         shape = tuple(read_object["shape"])
                         cur_player = int(read_object.get("cur_player"))
                         game_name = command_key
-                        #game_instance = self.imports.get(game_name)["Game"]
-                        #dict_functions = self.lambdas.get(game_name)
-                        board = np.array(array, dtype=dtype).reshape(shape)
-                        action = np.argmax(func(game.getCanonicalForm(board, cur_player)))
-                        print(action)
-                        #if dict_functions is None:
-                        #    print(f"No lambda functions found for game: {game_name}")
-                        #    continue
-
-                        #func = dict_functions.get(lobby.difficulty)
-                        #if func is None:
-                        #    print(f"No lambda function found for difficulty: {lobby.difficulty}")
-                        #    continue
-
-                        #move = None
-                        #valids = game_instance.getValidMoves(board, cur_player)
-                        #print("AI SEARCHING")
-                        #while True:
-                        #    x = game_instance.getCanonicalForm(board, cur_player)
-                        #    try:
-                        #        move = np.argmax(func(x))
-                        #        if move >= len(valids):
-                        #            print("Invalid move, retrying")
-                        #            continue
-                        #    except Exception as e:
-                        #        print(x.shape, x)
-                        #        print(f"Error in lambda function: {e}")
-                        #        break
-                        #    break
-                        #if move is not None:
-                         #   print("AI move was:", move)
                         await self.send_cmd(lobby.game_client, "play", "make_move",
                                                 {"move": int(action), "p_pos": "p1" if cur_player == 1 else "p2"})
                     case "draw":
---
+++ Tools/dynamic_imports.py
@@ -0,0 +1,155 @@
+class Importer:
+    def __init__(self):
+        pass
+
+    # import all games from /Games folder and create a instance of it
+    # import all NNet.py of each game
+    # find directory path of .h5 files of each game
+    # find name of .h5 file of each game
+    # create all lambda functions for ai operation for each difficulty (EDifficulty)
+    # define game_client specific method for imports if necessary
+    # loading as fast as possible login of game_client can be delayed
+    # define server specific method for imports if necessary
+
+    """
+    @staticmethod
+    def import_class_from_file(filepath: str, class_name: str):
+        spec = importlib.util.spec_from_file_location(class_name, filepath)
+        module = importlib.util.module_from_spec(spec)
+        spec.loader.exec_module(module)
+
+        # Find the class with case-insensitive match
+        for attr in dir(module):
+            if attr.lower() == class_name.lower():
+                return getattr(module, attr)
+
+        raise AttributeError(f"Class {class_name} not found in {filepath}")
+
+    def find_and_import_classes(self, games_folder: str):
+        imports = {}
+
+        for game_name in os.listdir(games_folder):
+            game_path = os.path.join(games_folder, game_name)
+            if os.path.isdir(game_path):
+                imports[game_name] = {}
+
+                # Import Game classes (files ending with Game.py)
+                for file_name in os.listdir(game_path):
+                    if file_name.endswith('Game.py'):
+                        game_file = os.path.join(game_path, file_name)
+                        if os.path.isfile(game_file):
+                            try:
+                                game_class = self.import_class_from_file(game_file, file_name.replace(".py", ""))
+                                imports[game_name]['Game'] = game_class()
+                            except (FileNotFoundError, AttributeError) as e:
+                                print(f"Could not import Game class from {game_file}: {e}")
+
+                # Import NNetWrapper class from keras/NNet.py
+                keras_file = os.path.join(game_path, 'keras', 'NNet.py')
+                if os.path.isfile(keras_file):
+                    try:
+                        keras_class = self.import_class_from_file(keras_file, 'NNetWrapper')
+                        imports[game_name]['NNetWrapper'] = keras_class
+                    except (FileNotFoundError, AttributeError) as e:
+                        print(f"Could not import NNetWrapper class from {keras_file}: {e}")
+
+                # import Keras folder and model
+                keras_model = os.path.join(game_path, 'keras', 'best.h5')
+                if os.path.isfile(keras_model):
+                    file = os.path.basename(keras_model)
+                    folder = os.path.dirname(os.path.abspath(keras_model))
+                    imports[game_name]["folder"] = folder
+                    imports[game_name]["file"] = file
+
+                # Import NNetWrapper class from pytorch/NNet.py
+                pytorch_file = os.path.join(game_path, 'pytorch', 'NNet.py')
+                if os.path.isfile(pytorch_file):
+                    try:
+                        pytorch_class = self.import_class_from_file(pytorch_file, 'NNetWrapper')
+                        imports[game_name]['NNetWrapper'] = pytorch_class
+                    except (FileNotFoundError, AttributeError) as e:
+                        print(f"Could not import NNetWrapper class from {pytorch_file}: {e}")
+
+                # import pytorch folder and model
+                torch_model = os.path.join(game_path, 'pytorch', 'best.h5')
+                if os.path.isfile(torch_model):
+                    file = os.path.basename(torch_model)
+                    folder = os.path.dirname(os.path.abspath(torch_model))
+                    imports[game_name]["folder"] = folder
+                    imports[game_name]["file"] = file
+        return imports
+
+    def create_lambda(self):
+        print("Loading Ai models...")
+        results = {}
+        ignored_games = []
+        # save original stdout
+        original_stdout = sys.stdout
+        original_stderr = sys.stderr
+        try:
+            # redirect stdout to silent mode
+            sys.stdout = open(os.devnull, 'w')
+            sys.stderr = open(os.devnull, 'w')
+            for k, v in self.imports.items():
+                game = self.imports[k].get("Game")
+                network_class = self.imports[k].get("NNetWrapper")  # get the right NNet
+                h5_folder = self.imports[k].get("folder")  # get the .h5 trained model path
+                h5_file = self.imports[k].get("file")  # get the .h5 file
+                if h5_folder and h5_file:
+                    for difficulty in EDifficulty:
+                        mcts = self.init_nn(game, network_class, h5_folder, h5_file, difficulty)
+                        if k not in results:
+                            results[k] = {}
+                        results[k][difficulty] = lambda x: mcts.getActionProb(x, temp=0)
+                else:
+                    ignored_games.append(f"For '{game}' no best.h5 was found. Ignoring it.")
+        finally:
+            # set stdout back to original
+            sys.stdout = original_stdout
+            sys.stderr = original_stderr
+            print("Loading Ai models done.")
+        return results
+    
+    """
+
+
+
+    """
+    loading and creation of MCTS function
+    
+    
+    def init_nn(game, nnet, folder: str, file: str, difficutly: EDifficulty):
+        nn = nnet(game)
+        nn.load_checkpoint(folder, file)
+        args = dotdict({'numMCTSSims': difficutly.value, 'cpuct': 1.0})
+        mcts = MCTS(game, nn, args)
+        return mcts
+    
+    """
+
+    """
+     copy of game_ client opperation
+     
+     
+    @staticmethod
+    def import_game_classes(directory):
+        pattern: str = "Game.py"
+        imported_classes = {}
+
+        for root, _, files in os.walk(directory):
+            for filename in files:
+                if filename.endswith(pattern):
+                    module_name = filename[:-3]
+                    file_path = os.path.join(root, filename)
+
+                    spec = importlib.util.spec_from_file_location(module_name, file_path)
+                    module = importlib.util.module_from_spec(spec)
+                    spec.loader.exec_module(module)
+
+                    for name, obj in inspect.getmembers(module, inspect.isclass):
+                        name = name.replace("Game", "").lower()
+                        if obj.__module__ == module_name:
+                            imported_classes[name] = obj
+                            print("Imported: ", name)
+        return imported_classes
+    """
