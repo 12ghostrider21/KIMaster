b2034036752d92da161eba16e171028c4a9893e6
S-Reinhard <75866827+S-Reinhard@users.noreply.github.com>
2024-06-15 17:39:22 +0200

    adding .h5 to crawler, reduce repetitive code, write docstring

--- Server/socketServer.py
+++ Server/socketServer.py
@@ -1,9 +1,10 @@
 import importlib
 import io
-import os
-
-import numpy as np
 import pygame
+from logging import warn
+from os import walk, environ, listdir
+from os.path import join
+import numpy as np
 from fastapi import WebSocket, WebSocketDisconnect
 
 from Tools.i_game import IGame
@@ -15,14 +16,14 @@ from connection_manager import AbstractConnectionManager
 from lobby import Lobby
 from lobby_manager import LobbyManager
 
-os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'  # disable some logging features
+environ['TF_CPP_MIN_LOG_LEVEL'] = '3'  # disable some logging features
 
 
 class SocketServer(AbstractConnectionManager):
     def __init__(self, msg_builder: LanguageHandler):
         super().__init__(msg_builder)
         self.manager: LobbyManager = LobbyManager()
-        self.game_name, self.game_instances, self.game_nnet = self.crawl_game_files()
+        self.game_name, self.game_instances, self.game_nnet, self.game_h5 = self.crawl_game_files()
 
         self.game: IGame = self.dynamic_import("Games.connect4.Connect4Game", "Connect4Game")()
         nnet = self.dynamic_import('Games.connect4.keras.NNet', 'NNetWrapper')
@@ -63,64 +64,108 @@ class SocketServer(AbstractConnectionManager):
         png_bytes = byte_io.getvalue()
         byte_io.close()
         return png_bytes
+    
+    def __crawler_helper(found_files: list[str], current_game: str, root:str, target_dict: dict[str, str], ignored: list[str], pattern:str) -> bool :
+        """
+    Helper function to update the target dictionary with the found files if they are unique. 
 
-    def crawl_game_files(self):
+    Args:
+        found_files (list[str]): List of files found matching the pattern.
+        current_game (str): The current game being processed.
+        root (str): The root directory where the files are being searched.
+        target_dict (dict[str, str]): The dictionary to update with the found file path.
+        ignored (list[str]): List of games to be ignored due to issues.
+        pattern (str): The pattern used for matching the files.
+
+    Returns:
+        bool: True if the file is unique and added to the target_dict, False otherwise.
+    
+    Raises:
+        Warning: If multiple Files with identical pattern are found in the GameDirectory
+    
+    Notes:
+        The intention behind this function is to reduce repetitive code in the `crawl_game_files()` function
+    
+    See Also:
+        `crawl_game_files()`
+    
+    """
+        if len(found_files) == 1: # add found file to target if it is unique
+            target_dict.update({current_game:join(root,found_files[0])})
+        elif len(found_files) > 1: # ignore it if it is not unique
+            ignored.append(current_game)
+            warn(f"could not identify unique {pattern} file for game: {current_game}. Make sure that there is only one file matching the name NNet.py in the {root} directory. To prevent unpredictable behaviour {current_game} will be ignored.")
+            return False
+        return True
+
+    def crawl_game_files(self) -> tuple[list[str], dict[str:str], dict[str:str], dict[str:str]]:
+        """
+    Crawl the game files and categorize them into different dictionaries based on their types.
+
+    Returns:
+        tuple: A tuple containing four elements:
+            - games (list[str]): List of all game names found in the directory.
+            - game_pys (dict[str, str]): Dictionary with game names as keys and paths to their Game.py files as values.
+            - game_nnets (dict[str, str]): Dictionary with game names as keys and paths to their NNet.py files as values.
+            - game_h5s (dict[str, str]): Dictionary with game names as keys and paths to their .h5 files as values.
+
+    Raises:
+        Warning: If multiple directories with identical names are found in the GameDirectory.
+    """
+        # init return values
         game_pys: dict[str, str] = {}
-        game_nnets: dict[str, str] = {}
-        games: list[str] = [game for game in os.listdir("/app/Games")]
+        game_nnets: dict[str,str] = {}
+        game_h5s: dict[str, str] = {}
+        games:list[str] = [game for game in listdir("/app/Games")]
+        results = (games, game_pys, game_nnets, game_h5s)  
+        
+        # init list for those games to exclude from result
         ignored: list[str] = []
-
-        for root, dir_names, file_names in os.walk("/app/Games"):
+        
+        for root, dir_names, file_names in walk("Games"):
             # make all lower case, to remove case sensitivity
-
+            
             # get the current game
             current_game = [game for game in games if game.lower() in root.lower()]
-
+            
             # only resolve Files if a unique game could be identified
             if len(current_game) == 1:
                 current_game = current_game[0]
-
+                
                 # find *Game.py file of the current game
-                game_py_files = [f for f in file_names if f.lower().endswith("Game.py".lower())]
-                if len(game_py_files) == 1:
-                    game_pys.update({current_game: os.path.join(root, game_py_files[0])})
-                elif len(game_py_files) > 1:
-                    ignored.append(current_game)
-                    print(
-                        f"could not identify unique *Game.py file for game: {current_game}. Make sure that there is only one file matching the name *Game.py in the {root} directory. To prevent unpredictable behaviour {current_game} will be ignored.")
+                game_py_file_pattern: str = "Game.py"
+                found_game_pys = [f for f in file_names if f.lower().endswith(game_py_file_pattern.lower())]
+                if not self.__crawler_helper(found_game_pys, current_game, root, game_pys, ignored, "*"+game_py_file_pattern):
                     continue
-
-                # find the NNet.py of the current game
-                nnet_files = [f for f in file_names if f.lower() == "NNet.py".lower()]
-                if len(nnet_files) == 1:
-                    game_nnets.update({current_game: os.path.join(root, nnet_files[0])})
-                elif len(nnet_files) > 1:
-                    ignored.append(current_game)
-                    print(
-                        f"could not identify unique NNet.py file for game: {current_game}. Make sure that there is only one file matching the name NNet.py in the {root} directory. To prevent unpredictable behaviour {current_game} will be ignored.")
+                
+                # find NNet.py files of the current game
+                nnet_file_pattern = "NNet.py"
+                found_nnets = [f for f in file_names if f.lower() == nnet_file_pattern.lower()]
+                if not self.__crawler_helper(found_nnets, current_game, root, game_nnets, ignored, nnet_file_pattern):
                     continue
-
+                
+                # find the .h5 of the current game
+                h5_file_pattern = ".h5"
+                found_h5s = [f for f in file_names if f.lower().endswith(h5_file_pattern.lower())]
+                if not self.__crawler_helper(found_h5s, current_game, root, game_h5s, ignored, "*"+h5_file_pattern):
+                    continue
+                
             elif len(current_game) > 1:
-                print(
-                    f"the game {current_game} seems to exist several times. To prevent unpredictable behaviour {current_game} will be ignored. This Error is caused if there are multiple directories with identical names in your GameDirectory.")
+                warn(f"the game {current_game} seems to exist several times. To prevent unpredictable behaviour {current_game} will be ignored. This Error is caused if there are multiple directories with identical names in your GameDirectory.")
                 for game in current_game:
-                    ignored.append(current_game)
-
+                    ignored.append(game)
+        
         # remove ignored games
-        for game in ignored:
-            try:
-                games.remove(game)
-            except ValueError:
-                pass
-            try:
-                game_pys.pop(game)
-            except KeyError:
-                pass
-            try:
-                game_nnets.pop(game)
-            except KeyError:
-                pass
-        return games, game_pys, game_nnets
+        for ignored_game in ignored:
+            for r in results:
+                try:
+                    if isinstance(r, dict):
+                        r.pop(ignored_game)
+                    elif isinstance(r, list):
+                        r.remove(ignored_game)
+                except (ValueError, KeyError):
+                    pass
+        return results
 
     async def websocket_endpoint(self, websocket: WebSocket):
         await self.connect(websocket)
