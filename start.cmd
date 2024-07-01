a60192665ab50b772aa4fcdb5cb817ae40f11cdd
Flying_Suricate <suricate2505@gmail.com>
2024-07-01 20:16:06 +0200

    add test to start.cmd

--- start.cmd
+++ start.cmd
@@ -1,4 +1,4 @@
-docker-compose kill swtp-server swtp-frontend
+docker-compose kill swtp-server swtp-frontend swtp-frontend-debug
 docker-compose build
-docker-compose up -d swtp-server swtp-frontend
+docker-compose up -d swtp-server swtp-frontend swtp-frontend-debug
 docker image prune -f
