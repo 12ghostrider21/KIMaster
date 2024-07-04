2eaf499668b18cf402c4f5b5cae95595d955e4d3
Thorben Jones <thorbenjones@web.de>
2024-07-04 14:10:43 +0200

    Updated gameRules with OthelloRules with translations

---
+++ Frontend/src/components/gameRules/OthelloRules.vue
@@ -0,0 +1,34 @@
+<template>
+    <div>
+      <h2>{{ $t('rules.othello.title') }}</h2>
+      <p>{{ $t('rules.othello.description') }}</p>
+  
+      <h3>{{ $t('rules.othello.setup.title') }}</h3>
+      <ul>
+        <li>{{ $t('rules.othello.setup.point1') }}</li>
+        <li>{{ $t('rules.othello.setup.point2') }}</li>
+        <li>{{ $t('rules.othello.setup.point3') }}</li>
+      </ul>
+  
+      <h3>{{ $t('rules.othello.gameplay.title') }}</h3>
+      <ul>
+        <li>{{ $t('rules.othello.gameplay.point1') }}</li>
+        <li>{{ $t('rules.othello.gameplay.point2') }}</li>
+        <li>{{ $t('rules.othello.gameplay.point3') }}</li>
+        <li>{{ $t('rules.othello.gameplay.point4') }}</li>
+      </ul>
+  
+      <h3>{{ $t('rules.othello.endgame.title') }}</h3>
+      <ul>
+        <li>{{ $t('rules.othello.endgame.point1') }}</li>
+        <li>{{ $t('rules.othello.endgame.point2') }}</li>
+      </ul>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    name: 'OthelloRules'
+  };
+  </script>
+  
