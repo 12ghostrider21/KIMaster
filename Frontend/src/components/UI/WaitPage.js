4ae869de982e66cca9ed22e15aa0344e3c96b47f
Omar Karkotli <karkotlio@yahoo.de>
2024-07-10 21:28:27 +0200

    waitingPage gestylt

---
+++ Frontend/src/components/UI/WaitPage.js
@@ -0,0 +1,78 @@
+
+import { mapActions, mapGetters } from 'vuex';
+import * as ENUMS from '../enums';
+
+export default {
+  data() {
+    return {
+      difficulty: 'hard',
+    };
+  },
+  computed: {
+    ...mapGetters(['gameActive', 'position', 'game', 'callPos', 'positionsInLobby']),
+    positionSelect: {
+      get() {
+        return this.position;
+      },
+      set(value) {
+        this.updatePosition(value);
+      }
+    }
+  },
+  beforeMount() {
+    this.lobbyPos();
+    this.lobbyStatus();
+  },
+  mounted() {
+    if (this.gameActive) {
+      this.goToGame();
+    }
+  },
+  methods: {
+    ...mapActions(['initWebSocket', 'sendWebSocketMessage', 'updatePosition']),
+    sendMessage(data) {
+      console.log(data);
+      this.sendWebSocketMessage(JSON.stringify(data));
+    },
+    lobbyPos() {
+      const data = {
+        command: 'lobby',
+        command_key: 'pos',
+      };
+      this.sendMessage(data);
+    },
+    lobbyStatus() {
+      const data = {
+        command: "lobby",
+        command_key: "status",
+      };
+      this.sendMessage(data);
+    },
+    swapPositionInLobby() {
+      const data = {
+        command: 'lobby',
+        command_key: 'swap',
+        pos: this.positionSelect
+      };
+      this.sendMessage(data);
+    },
+    goToGame() {
+      this.$router.push({
+        name: 'play',
+        params: { game: this.game }
+      });
+    },
+  },
+  watch: {
+    gameActive(newVal) {
+      if (newVal) {
+        this.goToGame();
+      }
+    },
+    callPos(newVal) {
+      this.lobbyPos();
+      this.lobbyStatus();
+    },
+  },
+};
+
