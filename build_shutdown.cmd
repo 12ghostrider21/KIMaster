c26fd4061dce42ee4bddb955ef971b94dc39b83c
Flying-Suricate <suricate2505@gmail.com>
2024-07-10 20:05:37 +0200

    remove LFS

--- build_shutdown.cmd
+++
@@ -1,6 +0,0 @@
-docker-compose kill swtp-server swtp-frontend swtp-frontend-debug
-docker-compose build
-docker-compose up -d swtp-server swtp-frontend swtp-frontend-debug
-docker image prune -f
-
-shutdown /s /t 10
