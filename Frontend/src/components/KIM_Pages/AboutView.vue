f6324077eca35d08f4485cd51472b3edc5e752df
Omar Karkotli <karkotlio@yahoo.de>
2024-07-24 18:58:22 +0200

    Datenschutzerklärung eingerichtet

--- Frontend/src/components/KIM_Pages/AboutView.vue
+++ Frontend/src/components/KIM_Pages/AboutView.vue
@@ -1,6 +1,96 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <h1>Datenschutzerklärung</h1>
+    <div class="privacy-policy">
+      <p><strong>A. Name und Anschrift des Verantwortlichen und seines Vertreters</strong></p>
+      <p>Der Verantwortliche im Sinne der EU-Datenschutzgrundverordnung (DSGVO) und anderer nationaler Datenschutzgesetze - insbesondere des Hessischen Datenschutz- und Informationsfreiheitsgesetzes (HDSIG) - sowie sonstiger datenschutzrechtlicher Bestimmungen ist die:</p>
+      <p>Technische Hochschule Mittelhessen (THM)<br>
+      Wiesenstraße 14<br>
+      Tel.: + 49 641 309-0<br>
+      E-Mail: info@thm.de<br>
+      Gesetzlich vertreten durch ihren Präsidenten<br>
+      Prof. Dr. Matthias Willems<br>
+      Wiesenstraße 14<br>
+      Tel.: + 49 641 309-0<br>
+      E-Mail: praesident@thm.de</p>
+
+      <p><strong>B. Kontaktdaten des Datenschutzbeauftragten</strong></p>
+      <p>Die Datenschutzbeauftragten des Verantwortlichen sind:</p>
+      <p>Ingmar Böffel (Recht)<br>
+      Prof. Thomas Friedl (Technik)<br>
+      Wiesenstraße 14<br>
+      Tel.: + 49 641 309-0<br>
+      E-Mail: datenschutzbeauftragter@thm.de</p>
+
+      <p><strong>C. Allgemeines zur Datenverarbeitung</strong></p>
+      <p>1. Umfang der Verarbeitung personenbezogener Daten</p>
+      <p>Personenbezogene Daten sind Informationen mit deren Hilfe eine natürliche Person bestimmbar ist also Angaben durch die Personen identifizierbar sind. Dazu gehören insbesondere Namen E-Mail-Adressen Matrikelnummern oder Telefonnummern. Aber auch Daten über Vorlieben Hobbies Mitgliedschaften oder auch Informationen über Webseiten die aufgesucht wurden zählen zu personenbezogenen Daten.</p>
+      <p>Die Verarbeitung personenbezogener Daten erfolgt stets im Einklang mit der Datenschutz-Grundverordnung (DSGVO) und dem Hessischen Datenschutz- und Informationsfreiheitsgesetz (HDSIG) sowie ggf. den einschlägigen Bestimmungen weiterer Gesetze insbesondere des Hessischen Hochschulgesetzes (HHG).</p>
+      <p>Wir erheben und verarbeiten personenbezogene Daten unserer Nutzer grundsätzlich nur soweit dies zur Bereitstellung einer funktionsfähigen Website erforderlich ist.</p>
+
+      <p><strong>D. Bereitstellung der Website und Erstellung von Logfiles</strong></p>
+      <p>1. Umfang der Datenverarbeitung</p>
+      <p>Bei jedem Aufruf unserer Internetseite erfasst unser System automatisiert Daten und Informationen vom Computersystem des aufrufenden Rechners. Folgende Daten werden hierbei erhoben:</p>
+      <ul>
+        <li>Informationen über den Browsertyp und die verwendete Version</li>
+        <li>Das Betriebssystem des Nutzers</li>
+        <li>Den Internet-Service-Provider des Nutzers</li>
+        <li>Die IP-Adresse des Nutzers</li>
+        <li>Datum und Uhrzeit des Zugriffs</li>
+        <li>Websites von denen das System des Nutzers auf unsere Internetseite gelangt</li>
+        <li>Websites die vom System des Nutzers über unsere Website aufgerufen werden</li>
+      </ul>
+      <p>Die Daten werden ebenfalls in den Logfiles unseres Systems gespeichert. Eine Speicherung dieser Daten zusammen mit anderen personenbezogenen Daten des Nutzers findet nicht statt.</p>
+      <p>2. Rechtsgrundlage für die Verarbeitung</p>
+      <p>Rechtsgrundlage für die vorübergehende Speicherung der Daten und der Logfiles ist Art. 6 Abs. 1 lit. f DSGVO.</p>
+      <p>3. Zweck der Verarbeitung</p>
+      <p>Die vorübergehende Speicherung der IP-Adresse durch das System ist notwendig um eine Auslieferung der Website an den Rechner des Nutzers zu ermöglichen. Hierfür muss die IP-Adresse des Nutzers für die Dauer der Sitzung gespeichert bleiben.</p>
+      <p>Die Speicherung in Logfiles erfolgt um die Funktionsfähigkeit der Website sicherzustellen. Zudem dienen uns die Daten zur Optimierung der Website und zur Sicherstellung der Sicherheit unserer informationstechnischen Systeme. Eine Auswertung der Daten zu Marketingzwecken findet in diesem Zusammenhang nicht statt.</p>
+      <p>In diesen Zwecken liegt auch unser berechtigtes Interesse an der Datenverarbeitung nach Art. 6 Abs. 1 lit. f DSGVO.</p>
+      <p>4. Dauer der Speicherung der personenbezogenen Daten</p>
+      <p>Die Daten werden gelöscht wenn die jeweilige Sitzung beendet ist.</p>
+      <p>Im Falle einer Speicherung der Daten in Logfiles werden diese nach spätestens 14 Tagen gelöscht. Eine darüberhinausgehende Speicherung ist möglich. In diesem Fall werden die IP-Adressen der Nutzer gelöscht oder verfremdet sodass eine Zuordnung des aufrufenden Clients nicht mehr möglich ist.</p>
+      <p>Die Erfassung der Daten zur Bereitstellung der Website und die Speicherung der Daten in Logfiles sind für den Betrieb der Internetseite zwingend erforderlich. Es besteht folglich seitens des Nutzers keine Widerspruchsmöglichkeit.</p>
+
+      <p>Der Schutz Ihrer persönlichen Daten ist uns ein wichtiges Anliegen. In dieser Datenschutzerklärung informieren wir Sie darüber welche Daten wir erheben wie wir diese verarbeiten und welche Rechte Sie in Bezug auf Ihre Daten haben.</p>
+
+      <p><strong>E. Verwendung von Cookies</strong></p>
+      <p>1. Umfang der Datenverarbeitung</p>
+      <p>Unsere Website verwendet Cookies. Bei Cookies handelt es sich um Textdateien die im Internetbrowser bzw. vom Internetbrowser auf dem Computersystem des Nutzers gespeichert werden. Ruft ein Nutzer eine Website auf so kann ein Cookie auf dem Betriebssystem des Nutzers gespeichert werden. Dieser Cookie enthält eine charakteristische Zeichenfolge die eine eindeutige Identifizierung des Browsers beim erneuten Aufrufen der Website ermöglicht.</p>
+      <p>Wir setzen keine Cookies ein und es findet kein Tracking von Nutzerdaten statt.</p>
+
+      <p><strong>F. Verwendung von externen Links</strong></p>
+      <p>1. Umfang der Datenverarbeitung</p>
+      <p>Unsere Website kann Links zu anderen Webseiten enthalten auf deren Datenschutzpraktiken wir keinen Einfluss haben. Wir empfehlen Ihnen die Datenschutzerklärungen dieser externen Seiten in diesem Fall zu konsultieren und zu überprüfen.</p>
+
+      <p><strong>G. Rechte der betroffenen Person</strong></p>
+      <p>Werden Ihre personenbezogenen Daten verarbeitet stehen Ihnen als betroffener Person i.S.d. DSGVO folgende Rechte gegenüber dem Verantwortlichen zu:</p>
+      <ul>
+        <li>Auskunftsrecht: Sie können von dem Verantwortlichen eine Bestätigung darüber verlangen ob personenbezogene Daten die Sie betreffen verarbeitet werden.</li>
+        <li>Recht auf Berichtigung: Sie haben ein Recht auf Berichtigung und/oder Vervollständigung Ihrer Daten gegenüber dem Verantwortlichen sofern die verarbeiteten personenbezogenen Daten die Sie betreffen unrichtig oder unvollständig sind. Der Verantwortliche hat die Berichtigung unverzüglich vorzunehmen.</li>
+        <li>Recht auf Löschung: Sie können von dem Verantwortlichen verlangen dass die Sie betreffenden personenbezogenen Daten unverzüglich gelöscht werden. Der Verantwortliche ist verpflichtet diese Daten unverzüglich zu löschen sofern keine gesetzlichen Aufbewahrungspflichten entgegenstehen.</li>
+        <li>Recht auf Einschränkung der Verarbeitung: Unter bestimmten Voraussetzungen können Sie die Einschränkung der Verarbeitung Ihrer Daten verlangen.</li>
+        <li>Recht auf Unterrichtung: Haben Sie das Recht auf Berichtigung Löschung oder Einschränkung der Verarbeitung gegenüber dem Verantwortlichen geltend gemacht ist dieser verpflichtet allen Empfängern denen die Sie betreffenden personenbezogenen Daten offengelegt wurden diese Berichtigung oder Löschung der Daten oder Einschränkung der Verarbeitung mitzuteilen es sei denn dies erweist sich als unmöglich oder ist mit einem unverhältnismäßigen Aufwand verbunden.</li>
+        <li>Recht auf Datenübertragbarkeit: Sie haben das Recht die Sie betreffenden personenbezogenen Daten die Sie dem Verantwortlichen bereitgestellt haben in einem strukturierten gängigen und maschinenlesbaren Format zu erhalten. Außerdem haben Sie das Recht diese Daten einem anderen Verantwortlichen ohne Behinderung durch den Verantwortlichen dem die personenbezogenen Daten bereitgestellt wurden zu übermitteln</li>
+        <li>Widerspruchsrecht: Sie haben das Recht aus Gründen die sich aus ihrer besonderen Situation ergeben jederzeit gegen die Verarbeitung der Sie betreffenden personenbezogenen Daten die aufgrund von Art. 6 Abs. 1 lit. e) DSGVO erfolgt Widerspruch einzulegen; dies gilt auch für ein etwaiges auf diese Bestimmungen gestütztes Profiling.</li>
+        <li>Recht auf Widerruf der datenschutzrechtlichen Einwilligungserklärung: Sie haben das Recht Ihre datenschutzrechtliche Einwilligungserklärung jederzeit zu widerrufen. Durch den Widerruf der Einwilligung wird die Rechtmäßigkeit der aufgrund der Einwilligung bis zum Widerruf erfolgten Verarbeitung nicht berührt.</li>
+      </ul>
+
+      <p><strong>H. Änderungen der Datenschutzerklärung</strong></p>
+      <p>Wir behalten uns vor diese Datenschutzerklärung bei Bedarf anzupassen insbesondere um sie an Änderungen der rechtlichen Rahmenbedingungen anzupassen. Die jeweils aktuelle Version ist auf unserer Website verfügbar.</p>
+
+      <p><strong>I. Rechte und Beschwerdemöglichkeiten</strong></p>
+      <p>Unbeschadet eines anderweitigen verwaltungsrechtlichen oder gerichtlichen Rechtsbehelfs steht Ihnen das Recht auf Beschwerde bei einer Aufsichtsbehörde insbesondere in dem Mitgliedstaat ihres Aufenthaltsorts ihres Arbeitsplatzes oder des Orts des mutmaßlichen Verstoßes zu wenn Sie der Ansicht sind dass die Verarbeitung der Sie betreffenden personenbezogenen Daten gegen die EU- Datenschutzgrundverordnung verstößt.</p>
+      <p>Die Aufsichtsbehörde bei der die Beschwerde eingereicht wurde unterrichtet den Beschwerdeführer über den Stand und die Ergebnisse der Beschwerde einschließlich der Möglichkeit eines gerichtlichen Rechtsbehelfs nach Art. 78 DSGVO.</p>
+      <p>Sie haben das Recht sich bei datenschutzrechtlichen Problemen bei der zuständigen Aufsichtsbehörde des Landes Hessen zu beschweren.</p>
+      <p>Kontaktadresse der Fachaufsichtsbehörde der Technischen Hochschule Mittelhessen:</p>
+      <p>Der Hessische Datenschutzbeauftragte<br>
+      Postfach 3163<br>
+      65021 Wiesbaden<br>
+      E-Mail an HDSB<br>
+      Telefon: +49 611 1408 – 0<br>
+      Telefax: +49 611 1408 – 611</p>
+    </div>
   </div>
   <footer-bar></footer-bar>
 </template>
@@ -11,6 +101,14 @@
     min-height: 100vh;
     display: flex;
     align-items: center;
+    flex-direction: column;
+  }
+
+  .privacy-policy {
+    max-width: 800px;
+    margin: 0 auto;
+    padding: 20px;
+    text-align: left;
   }
 }
 </style>
